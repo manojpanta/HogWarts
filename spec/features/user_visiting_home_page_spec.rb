588203024e27065ea3c01a42aee61b064e7b8b74
manojpanta <manojpanta95@gmail.com>
2019-04-03 13:06:43 -0600

    added test for first user story

---
+++ spec/features/user_visiting_home_page_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+describe 'user visitng home page' do
+  scenario 'selects slytherin and click on students' do
+    it 'returns students' do
+      visit '/'
+      select 'Slytherin'
+      click_on 'Get Students'
+
+
+      expect(current_path).to eq('/search?house=slythrin')
+      expect(page).to have_css('.student', 22)
+    end
+  end
+end
+
+# As a user
+# When I visit "/"
+# And I select "Slytherin" from the dropdown
+# And I click on "Get Students"
+# Then my path should be "/search" with "house=slytherin" in the parameters
+# And I should see a message "22 Students"
+# And I should see a list of the 22 members of Slytherin
+# And I should see a name and id for each student.
+```
